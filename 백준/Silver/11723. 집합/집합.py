b0c56f1642dd3c9f48155c76d7ce6ec4a0fb72e7
dahyeong <da0108hy@naver.com>
2025-01-02 00:55:28 +0900

    [Silver V] Title: 집합, Time: 1884 ms, Memory: 128128 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,35 @@
+import sys
+n = int(sys.stdin.readline().strip())
+s = []
+
+for i in range(n):
+    l = sys.stdin.readline().strip().split()
+    if len(l)==2:
+        a, b = l[0], int(l[1])
+    else:
+        a = l[0]
+
+    if a == 'add':
+        if b in s:
+            continue
+        else:
+            s.append(b)
+    elif a == 'check':
+        if b in s:
+            print(1)
+        else:
+            print(0)
+    elif a == 'all':
+        s = set([i for i in range(1, 21)])
+    elif a == 'empty':
+        s = []
+    elif a == 'remove':
+        if b in s:
+            s.remove(b)
+        else:
+            continue
+    elif a == 'toggle':
+        if b in s:
+            s.remove(b)
+        else:
+            s.append(b)
